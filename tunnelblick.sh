fe8a7153d494a344cb710d5237367868d01295a6
tides <71273682+0xTides@users.noreply.github.com>
2023-03-03 13:55:14 +0000

    Update tunnelblick.sh

--- tunnelblick.sh
+++ tunnelblick.sh
@@ -7,17 +7,17 @@ if [ -d "/Applications/Tunnelblick.app" ]; then
     # Check if Tunnelblick.app is currently open
     if pgrep -x "Tunnelblick" >/dev/null; then
         echo "Tunnelblick is currently open. Quitting the app..."
-        osascript -e 'quit app "Tunnelblick"'
+        osascript -e 'tell application "Tunnelblick" to quit'
     fi
 
     # Uninstall Tunnelblick.app
     echo "Uninstalling Tunnelblick.app..."
-    rm -rf "/Applications/Tunnelblick.app"
-    rm -rf ~/Library/Application\ Support/Tunnelblick
-    rm -rf ~/Library/Application\ Support/Tunnelblick\ VPN\ Configurations
-    rm -rf ~/Library/Application\ Support/Tunnelblick\ User\ OpenVPN\ Configuration\ Files
-    rm -rf ~/Library/LaunchAgents/net.tunnelblick.tunnelblick.LaunchAtLogin.plist
-    rm -rf ~/Library/Logs/Tunnelblick.log
+    sudo rm -rf "/Applications/Tunnelblick.app"
+    sudo rm -rf ~/Library/Application\ Support/Tunnelblick
+    sudo rm -rf ~/Library/Application\ Support/Tunnelblick\ VPN\ Configurations
+    sudo rm -rf ~/Library/Application\ Support/Tunnelblick\ User\ OpenVPN\ Configuration\ Files
+    sudo rm -rf ~/Library/LaunchAgents/net.tunnelblick.tunnelblick.LaunchAtLogin.plist
+    sudo rm -rf ~/Library/Logs/Tunnelblick.log
     echo "Tunnelblick.app has been uninstalled."
 else
     echo "Tunnelblick.app not found."
