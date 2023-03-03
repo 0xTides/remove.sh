5dd06faf65e5a65467a778f597e0aa078d567cba
tides <71273682+0xTides@users.noreply.github.com>
2023-03-03 14:01:13 +0000

    Update tunnelblick.sh

--- tunnelblick.sh
+++ tunnelblick.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
+# Define the name of the Tunnelblick process
+tunnelblick_process="Tunnelblick"
+
 # Check if Tunnelblick.app exists
 if [ -d "/Applications/Tunnelblick.app" ]; then
     echo "Tunnelblick.app found."
 
-    # Check if Tunnelblick.app is currently open
-    if pgrep -x "Tunnelblick" >/dev/null; then
-        echo "Tunnelblick is currently open. Quitting the app..."
-        osascript -e 'tell application "Tunnelblick" to quit'
+    # Check if Tunnelblick is currently running
+    if pgrep -x "$tunnelblick_process" >/dev/null; then
+        echo "Tunnelblick is currently running. Killing the process..."
+        pkill "$tunnelblick_process"
     fi
 
     # Uninstall Tunnelblick.app
     echo "Uninstalling Tunnelblick.app..."
     sudo rm -rf "/Applications/Tunnelblick.app"
-    sudo rm -rf ~/Library/Application\ Support/Tunnelblick
-    sudo rm -rf ~/Library/Application\ Support/Tunnelblick\ VPN\ Configurations
-    sudo rm -rf ~/Library/Application\ Support/Tunnelblick\ User\ OpenVPN\ Configuration\ Files
-    sudo rm -rf ~/Library/LaunchAgents/net.tunnelblick.tunnelblick.LaunchAtLogin.plist
-    sudo rm -rf ~/Library/Logs/Tunnelblick.log
+    sudo rm -rf "$HOME/Library/Application Support/Tunnelblick"
+    sudo rm -rf "$HOME/Library/Application Support/Tunnelblick VPN Configurations"
+    sudo rm -rf "$HOME/Library/Application Support/Tunnelblick User OpenVPN Configuration Files"
+    sudo rm -rf "$HOME/Library/LaunchAgents/net.tunnelblick.tunnelblick.LaunchAtLogin.plist"
+    sudo rm -rf "$HOME/Library/Logs/Tunnelblick.log"
     echo "Tunnelblick.app has been uninstalled."
 else
     echo "Tunnelblick.app not found."
