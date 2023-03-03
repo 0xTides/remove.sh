3f14fbb110e82d83e73400d427067f3a6e5d114e
tides <71273682+0xTides@users.noreply.github.com>
2023-03-03 13:21:41 +0000

    Update tunnelblick.sh

--- tunnelblick.sh
+++ tunnelblick.sh
@@ -7,7 +7,9 @@ APPS_PATH="/Applications"
 if [ -d "$APPS_PATH/Tunnelblick.app" ]; then
     # Remove Tunnelblick.app
     rm -rf "$APPS_PATH/Tunnelblick.app"
-    echo "Tunnelblick.app has been removed."
+    # Display a pop-up notification
+    osascript -e 'display notification "Tunnelblick.app has been uninstalled." with title "Uninstall Complete"'
 else
+    # Do not display a pop-up notification
     echo "Tunnelblick.app was not found in the Applications folder."
 fi
