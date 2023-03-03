46fb6bc3d6cf57d7ac36bbf4082fd31fdcc1c407
tides <71273682+0xTides@users.noreply.github.com>
2023-03-03 13:52:42 +0000

    Update tunnelblick.sh

--- tunnelblick.sh
+++ tunnelblick.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-# Set the path to the Applications folder
-APPS_PATH="/Applications"
+# Check if Tunnelblick.app exists
+if [ -d "/Applications/Tunnelblick.app" ]; then
+    echo "Tunnelblick.app found."
 
-# Check if Tunnelblick.app exists in the Applications folder
-if [ -d "$APPS_PATH/Tunnelblick.app" ]; then
-    # Remove Tunnelblick.app
-    rm -rf "$APPS_PATH/Tunnelblick.app"
-    # Display a dialog box to the user
-    osascript -e 'tell application "System Events" to display dialog "Tunnelblick.app has been uninstalled." with title "Uninstall Complete" buttons {"OK"} default button 1'
+    # Check if Tunnelblick.app is currently open
+    if pgrep -x "Tunnelblick" >/dev/null; then
+        echo "Tunnelblick is currently open. Quitting the app..."
+        osascript -e 'quit app "Tunnelblick"'
+    fi
+
+    # Uninstall Tunnelblick.app
+    echo "Uninstalling Tunnelblick.app..."
+    rm -rf "/Applications/Tunnelblick.app"
+    rm -rf ~/Library/Application\ Support/Tunnelblick
+    rm -rf ~/Library/Application\ Support/Tunnelblick\ VPN\ Configurations
+    rm -rf ~/Library/Application\ Support/Tunnelblick\ User\ OpenVPN\ Configuration\ Files
+    rm -rf ~/Library/LaunchAgents/net.tunnelblick.tunnelblick.LaunchAtLogin.plist
+    rm -rf ~/Library/Logs/Tunnelblick.log
+    echo "Tunnelblick.app has been uninstalled."
 else
-    # Display a dialog box to the user
-    osascript -e 'tell application "System Events" to display dialog "Tunnelblick.app was not found in the Applications folder." with title "Uninstall Complete" buttons {"OK"} default button 1'
+    echo "Tunnelblick.app not found."
 fi
